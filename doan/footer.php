91167aea1be76049c6c3ba675c2d47c1b092047b
hyybee <94682500+hyybee@users.noreply.github.com>
2021-11-19 17:25:55 +0700

    Add files via upload

---
+++ doan/footer.php
@@ -0,0 +1,20 @@
+<div class="jumbotron" >
+<div class="appeal">  
+<h2>Xin chào các bạn đã đến với website BlackJack</h2>
+<table>
+         <ul>
+         <li><a class="center" href="#">Page 3</a></li>
+          <li><a href="#">Page 4</a></li>
+          <li><a href="#">Page 5</a></li>
+          <li><a href="#">Page 3</a></li>
+         </ul>
+ </table>     
+ </div>
+</div>
+<div>
+    
+</div>
+
+<!--/. Container-->
+</body>
+</html>
---
+++ doan/quangcao.php
@@ -0,0 +1,13 @@
+<div class="jumbotron" >
+<div class="appeal">  
+<h2>Xin chào các bạn đã đến với website BlackJack</h2>
+ </div>
+</div>
+<div>
+
+   
+</div>
+
+<!--/. Container-->
+</body>
+</html>
